19249e5126fbc5cb9f9ca533e44bd175fcad9c7b
spagnon7 <spagnon7@gmail.com>
2021-02-28 14:33:04 -0800

    create title and search input in appBar

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,56 @@ import ProductsList from './ProductsList';
 import Product from './Product';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
-import {setCategoriesListDisplay} from '../actions/appActions'
+import {setCategoriesListDisplay} from '../actions/appActions';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
-const useStyles = makeStyles(() => ({
+
+const useStyles = makeStyles((theme) => ({
   appBar: {
     marginBottom: 30
+  },
+  appToolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  homeButton: {
+    marginRight: 20,
+    display: 'inline-block'
+  },
+  search: {
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {backgroundColor: fade(theme.palette.common.white, 0.25)},
+    borderRadius: 5,
+    justifySelf: 'flex-end',
+    display: 'flex',
+    alignItems: 'center'
+  },
+    leftAppBarItems: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  searchIcon: {
+    height: '100%',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    padding: 5,
+    position: 'absolute',
+  },
+  searchInput: {
+    color: 'inherit',
+    padding: 5,
+    paddingLeft: 'calc(1em + 15px)',
+    width: '10em',
+    transition: 'width 0.3s'
+  },
+  searchInputFocused: {
+    width: '15em',
   }
 }));
 
@@ -43,10 +85,21 @@ function App() {
   return (
     <div id='app'>
       <AppBar position='static' className={classes.appBar}>
-        <Toolbar>
-          <IconButton color='inherit' onClick={() => {dispatch(setCategoriesListDisplay())}}>
-            <HomeIcon />
-          </IconButton>
+        <Toolbar className={classes.appToolbar}>
+          <div className={classes.leftAppBarItems}>
+            <IconButton color='inherit' className={classes.homeButton} onClick={() => {dispatch(setCategoriesListDisplay())}}>
+              <HomeIcon />
+            </IconButton>
+            <Typography>
+              Sean's Wonderful WebStore
+            </Typography>
+          </div>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase placeholder='Search...' className={classes.searchInput} classes={{focused: classes.searchInputFocused}}/>
+          </div>
         </Toolbar>
       </AppBar>
       {pageContent()}
